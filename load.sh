7d761d193e9fc376b8fd468a3673188fb4975cf0
volodymyr.kovenko <volodymyr.kovenko@vimmi.net>
2021-03-01 17:28:36 +0200

    changes in script used for data loading

--- load.sh
+++ load.sh
@@ -1,7 +1,16 @@
-#loading cornell-movie-dataset data
+##loading cornell-movie-dataset data
+mkdir -p data
+mkdir -p data/cornell
 wget http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
-unzip cornell_movie_dialogs_corpus.zip -d notebooks
+unzip cornell_movie_dialogs_corpus.zip -d data/cornell
 rm cornell_movie_dialogs_corpus.zip
+
+#loading opensubs data
+mkdir -p data/opensubs
+wget -O en.tar.gz http://opus.lingfil.uu.se/download.php?f=OpenSubtitles/en.tar.gz
+unzip  en.tar.gz -d data/opensubs
+rm en.tar.gz
+
 #loading w2v embeddings
 wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
 mkdir -p "embeddings"
