23a30c3778a4fb0de98bf80fc739dbdf5a7f68e4
Volodymyr Kovenko <volodymyr.kovenko@vimmi.net>
2021-03-20 18:07:06 +0200

    changes

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -48,21 +48,26 @@ class TelegramBot:
         self.dict_params = {"beam": {"beam_size": 7},
                             "nucleus": {"top_p": 0.95, "temperature": 1},
                             }
+        self.setting_message = False
+        self.setting_to_change = None
         self.decoding_type = "greedy"
 
         @self.BOT.callback_query_handler(func=lambda call: True)
         def callback_worker(call):
-            if call.data == 'beam':
-                self.predict = predict_beam
-                self.decoding_strategy = self.beam_decoder
-            elif call.data == 'nucleus':
-                self.predict = predict_nucleus
-                self.decoding_strategy = self.nucleus_decoder
-            else:
-                self.predict = predict_greedy
-                self.decoding_strategy = self.greedy_decoder
+            if call.data in ['beam', 'nucleus', 'greedy']:
+                if call.data == 'beam':
+                    self.predict = predict_beam
+                    self.decoding_strategy = self.beam_decoder
+                elif call.data == 'nucleus':
+                    self.predict = predict_nucleus
+                    self.decoding_strategy = self.nucleus_decoder
+                elif call.data == 'greedy':
+                    self.predict = predict_greedy
+                    self.decoding_strategy = self.greedy_decoder
 
-            self.decoding_type = call.data
+                self.decoding_type = call.data
+            else:
+                self.setting_to_change = call.data
 
         @self.BOT.message_handler(content_types=['text'],
                                   )
@@ -81,6 +86,47 @@ class TelegramBot:
                                           .format(self.decoding_type, self.dict_params[self.decoding_type]))
                 else:
                     self.BOT.send_message(message.from_user.id, "No settings for greedy decoding")
+            elif message.text == '/change_default':
+                if self.decoding_type != 'greedy':
+                    keyboard = self.set_keys_options()
+                    self.BOT.send_message(message.from_user.id,
+                                          "Please choose the parameter to change and write the new value.",
+                                          reply_markup=keyboard)
+                    self.setting_message = True
+                else:
+                    self.BOT.send_message(message.from_user.id, "No settings for greedy decoding.")
+
+            elif self.setting_message:
+                try:
+                    param = float(message.text)
+                except Exception as e:
+                    param = None
+
+                if param:
+                    initial_param = param
+                    param = max(param, 0)
+                    if self.decoding_type == 'nucleus':
+                        param = min(param, 1.0)
+                        param = max(param, 0.1)
+                    else:
+                        param = max(int(param), 2)
+                    self.dict_params[self.decoding_type][self.setting_to_change] = param
+                    if initial_param != param:
+                        self.BOT.send_message(message.from_user.id, "Successfully set value of {0} "
+                                                                    "for param {1}. "
+                                                                    "Initial param passed by user ({1}={2}) "
+                                                                    "was changed to fir constraints. ".format(param,
+                                                                                                              self.setting_to_change,
+                                                                                                              initial_param))
+                    else:
+                        self.BOT.send_message(message.from_user.id, "Successfully set value of {0} "
+                                                                    "for param {1}".format(param,
+                                                                                           self.setting_to_change))
+                else:
+                    self.BOT.send_message(message.from_user.id, "The error occurred, please try again with "
+                                                                "command /change_default. ")
+
+                self.setting_message = False
             else:
                 processed_sentence = process_sentence(message.text, self.token_mapping,
                                                       max_len=self.max_len)
@@ -116,6 +162,14 @@ class TelegramBot:
                                                 callback_data='greedy')
         keyboard.add(key_greedy)
 
+        return keyboard
+
+    def set_keys_options(self):
+        keyboard = types.InlineKeyboardMarkup()
+        possible_keys = self.dict_params[self.decoding_type]
+        for key in possible_keys.keys():
+            keyboard.add(types.InlineKeyboardButton(text=key,
+                                                    callback_data=key))
         return keyboard
 
     def start(self):
